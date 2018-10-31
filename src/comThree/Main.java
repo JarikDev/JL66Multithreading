6da64430057e7081b2cf0941fc6f04d16a83b883
ivan4000 <sxln1@mail.ru>
2018-10-31 18:31:30 +0300

    JL66multithreads

---
+++ src/comThree/Main.java
@@ -0,0 +1,22 @@
+package comThree;
+
+
+public class Main {
+    public static void main(String[] args) {
+        new MyThread().start();
+        new MyThread().start();
+        new MyThread().start();
+
+
+    }
+}
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 400; i++) {
+            System.out.println("Thread name is - " + Thread.currentThread().getName() + " i = " + i);
+
+        }
+    }
+}
---
+++ src/comTwo/Main.java
@@ -0,0 +1,25 @@
+package comTwo;
+
+public class Main {
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        myThread.start();
+        System.out.println(Thread.currentThread().getName());
+        MyRunnable myRunnable=new MyRunnable();
+        Thread thread2=new Thread(myRunnable);
+        thread2.start();
+     }
+}
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+    }
+}
+class MyRunnable implements Runnable{
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+    }
+}
